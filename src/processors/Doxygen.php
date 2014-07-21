544977c95f7aa659d1840911c9cb1c5ccf6cfd91
Ekow Abaka Ainooson <jainooson@gmail.com>
2014-07-21 08:19:36 +0000

    Generating class information for doxygen

--- src/processors/Doxygen.php
+++ src/processors/Doxygen.php
@@ -3,10 +3,29 @@ namespace nyansapow\processors;
 
 class Doxygen extends \nyansapow\SiteProcessor
 {
+    protected function parseText($textNodes)
+    {
+        $html = '';
+        
+        foreach($textNodes->childNodes as $node)
+        {
+            switch($node->nodeType){
+                case XML_ELEMENT_NODE:
+                    $html .= $this->parseText($node);
+                    break;
+                case XML_TEXT_NODE:
+                    $html .= $node->textContent;
+                    break;
+            }
+        }
+        
+        return $html;
+    }
+    
     public function outputSite() 
     {
         // Search for the index.xml file
-        $indexXml = $this->getDir() . 'index.xml';
+        $indexXml = $this->getDir() . 'xml/index.xml';
         if(!file_exists($indexXml)) return;
         $this->setLayout('doxygen.mustache', true);
                 
@@ -17,16 +36,36 @@ class Doxygen extends \nyansapow\SiteProcessor
         
         // Get all the classes
         $classes = $index->xpath("/doxygenindex/compound[@kind='class']");
+        
+        // Keep separate classlist and classid arrays so as not to hurt the template engine
         $classesList = array();
+        $classIds = array();
         foreach($classes as $class)
         {
-            $classXml = simplexml_load_file($this->getDir() . $class['refid'] . '.xml');
-            $classesList[] = array(
-                'name' => $class->name,
-                'brief' => $classXml->compounddef->briefdescription->para
+            $classXml = simplexml_load_file($this->getDir() . "xml/{$class['refid']}.xml");
+            $parents = $classXml->compounddef->basecompoundref;
+            $classIds[] = $class['refid'];
+            $classDescription = array(
+                'name' => (string)$class->name,
+                'link' => (string)$class->name . ".html",
+                'brief' => $this->parseText(dom_import_simplexml($classXml->compounddef->briefdescription)),
+                'description' => $this->parseText(dom_import_simplexml($classXml->compounddef->detaileddescription)),
+                'access' => (string)$classXml->compounddef['prot'],
+                'parents' => array()
             );
+            
+            foreach($parents as $parent)
+            {
+                $classDescription['parents'][] = array(
+                    'name' => (string)$parent,
+                    'refid' => (string)$parent['refid']
+                );
+            }
+            
+            $classesList[] = $classDescription;
         }
         
+        //Write the index page lists summary of classes
         $m = new \Mustache_Engine();
         $sideMenuItems = $m->render(
             file_get_contents(self::$nyansapow->getHome() . "/themes/default/templates/doxygen_side_menu.mustache"),
@@ -54,7 +93,25 @@ class Doxygen extends \nyansapow\SiteProcessor
             )
         );        
         
-        //Write the index page lists summary of classes
-        
+        // Write detailed pages for all of the classes
+        foreach($classesList as $class)
+        {
+            $content = $m->render(
+                file_get_contents(self::$nyansapow->getHome() . "/themes/default/templates/doxygen_class.mustache"),
+                $class
+            );
+            
+        $this->outputPage(
+            $this->getDir() . $class['name'] . '.html',
+            array(
+                'body' => $content,
+                'side-list' => $sideMenuItems,
+                'page_title' => $class['name'],
+                'site_name' => $this->settings['site-name'],
+                'date' => date('jS F, Y H:i:s'),
+                'assets_location' => \nyansapow\SiteProcessor::getAssetsLocation($this->getDir())
+            )
+        );               
+        }
     }
 }
