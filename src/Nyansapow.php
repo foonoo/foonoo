a033252c342bc29c0572208e1d2b0e69c10ded2e
Ekow Abaka Ainooson <jainooson@gmail.com>
2014-11-11 14:17:38 +0000

    Changes to the nyansapow core

--- src/Nyansapow.php
+++ src/Nyansapow.php
@@ -29,7 +29,7 @@ class Nyansapow
         
         if($destination == '')
         {
-            $destination = getcwd() . "/_site";
+            $destination = getcwd() . "/output_site";
         }        
         
         $pageDetails = $this->getPageFiles($source);
