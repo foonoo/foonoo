0eee6e4d8e30dfa95fb7065dceb5f8d372676de4
Ekow Abaka Ainooson <jainooson@gmail.com>
2020-01-30 05:50:29 -0600

    Now injecting sitedata into template content by  default

--- src/content/TemplateContentFactory.php
+++ src/content/TemplateContentFactory.php
@@ -8,19 +8,31 @@ use ntentan\honam\TemplateRenderer;
 use nyansapow\content\ContentFactory;
 use nyansapow\content\Content;
 use nyansapow\content\TemplateContent;
+use nyansapow\events\EventDispatcher;
+use nyansapow\events\SiteWriteStarted;
 use nyansapow\sites\AbstractSite;
 
 class TemplateContentFactory implements ContentFactory
 {
     private $templateRenderer;
+    private $site;
 
-    public function __construct(TemplateRenderer $templateRenderer)
+    public function __construct(TemplateRenderer $templateRenderer, EventDispatcher $eventDispatcher)
     {
         $this->templateRenderer = $templateRenderer;
+        $eventDispatcher->addListener(
+            SiteWriteStarted::class,
+            function(SiteWriteStarted $event) {
+                $this->site = $event->getSite();
+            });
     }
 
     public function create(string $source, string $destination): Content
     {
-        return new TemplateContent($this->templateRenderer,$source, $destination);
+        $content = new TemplateContent($this->templateRenderer,$source, $destination);
+        if($this->site) {
+            $content->setData($this->site->getTemplateData($this->site->getDestinationPath($destination)));
+        }
+        return $content;
     }
 }
