6a22a46dee691af34e1dcc75f60328a210ac4dd7
Ekow Abaka Ainooson <jainooson@gmail.com>
2020-10-16 20:25:20 -0500

    Completed tag parser

--- tests/TagParserTest.php
+++ tests/TagParserTest.php
@@ -27,37 +27,39 @@ class TagParserTest extends TestCase
         return strtoupper($matches[1]);
     }
 
-//    public function testTagRegistration()
-//    {
-//        $response = $this->tagParser->parse("Hello [[caps world]], this is an interesting tag.");
-//        $this->assertEquals("Hello WORLD[], this is an interesting tag.\n", $response);
-//    }
-//
-//    public function testMultiTags()
-//    {
-//        $response = $this->tagParser->parse("Hello [[caps world]], this is an interesting [[caps tag]].");
-//        $this->assertEquals("Hello WORLD[], this is an interesting TAG[].\n", $response);
-//    }
-//
-//    public function testIncompleteTags()
-//    {
-//        $response = $this->tagParser->parse("Hello [[caps world, this is an interesting tag.");
-//        $this->assertEquals("Hello [[caps world, this is an interesting tag.\n", $response);
-//        $response = $this->tagParser->parse("Hello [[caps world]], this is an interesting [[caps tag.");
-//        $this->assertEquals("Hello WORLD[], this is an interesting [[caps tag.\n", $response);
-//        $response = $this->tagParser->parse("Hello [[caps world, this is an interesting [[caps tag]].");
-//        $this->assertEquals("Hello [[caps world, this is an interesting TAG[].\n", $response);
-//    }
-//
-//    public function testDefaultAttributes()
-//    {
-//        $response = $this->tagParser->parse("Hello [[caps world|augmented]], this is an interesting tag.");
-//        $this->assertEquals("Hello WORLD[__default:augmented ], this is an interesting tag.\n", $response);
-//    }
+    public function testTagRegistration()
+    {
+        $response = $this->tagParser->parse("Hello [[caps world]], this is an interesting tag.");
+        $this->assertEquals("Hello [WORLD]->[], this is an interesting tag.\n", $response);
+    }
+
+    public function testMultiTags()
+    {
+        $response = $this->tagParser->parse("Hello [[caps world]], this is an interesting [[caps tag]].");
+        $this->assertEquals("Hello [WORLD]->[], this is an interesting [TAG]->[].\n", $response);
+    }
+
+    public function testIncompleteTags()
+    {
+        $response = $this->tagParser->parse("Hello [[caps world, this is an interesting tag.");
+        $this->assertEquals("Hello [[caps world, this is an interesting tag.\n", $response);
+        $response = $this->tagParser->parse("Hello [[caps world]], this is an interesting [[caps tag.");
+        $this->assertEquals("Hello [WORLD]->[], this is an interesting [[caps tag.\n", $response);
+        $response = $this->tagParser->parse("Hello [[caps world, this is an interesting [[caps tag]].");
+        $this->assertEquals("Hello [[caps world, this is an interesting [TAG]->[].\n", $response);
+    }
+
+    public function testDefaultAttributes()
+    {
+        $response = $this->tagParser->parse("Hello [[caps world|augmented]], this is an interesting tag.");
+        $this->assertEquals("Hello [WORLD]->[__default:augmented ], this is an interesting tag.\n", $response);
+    }
 
     public function testAttributes()
     {
         $response = $this->tagParser->parse("Hello [[caps Args| key=value ]] arguments");
-        $this->assertEquals("Hello ARGS[key:value ] arguments", $response);
+        $this->assertEquals("Hello [ARGS]->[key:value ] arguments\n", $response);
+        $response = $this->tagParser->parse("Hello [[caps Args| key=value word=meaning ]] arguments");
+        $this->assertEquals("Hello [ARGS]->[key:value word:meaning ] arguments\n", $response);
     }
 }
