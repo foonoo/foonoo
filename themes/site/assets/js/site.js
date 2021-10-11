66e30a7bf22120b033c2f2dcf2381cd11c0301b0
Ekow Abaka Ainooson <jainooson@gmail.com>
2021-10-10 22:37:37 -0500

    Improving stylesheets

--- themes/site/assets/js/site.js
+++ themes/site/assets/js/site.js
@@ -1,7 +1,22 @@
 function resizeSideMenu() {
+     
+    // Automatically adjust the height of the side menu
+    // @todo find a way to prevent this from running when menu is hidden
+    
     let height = window.innerHeight || document.body.clientHeight;
-    document.getElementById('side-toc').style.height = (height - document.getElementById('banner-wrapper').clientHeight) + 'px';
+    height = height - document.getElementById('banner-wrapper').clientHeight;
+    document.getElementById('side-toc').style.height = height + 'px';
+    document.getElementById('side-toc-modal').style.height = height + 'px';
+}
+
+function toggleSideMenu() {
+    document.getElementById("side-toc").classList.toggle("active");
+    document.getElementById('side-toc-modal').classList.toggle("active");
 }
 
 window.addEventListener('load', resizeSideMenu);
 window.addEventListener('resize', resizeSideMenu);
+
+document.getElementById('hamburger').addEventListener('click', toggleSideMenu);
+document.getElementById('side-toc-modal').addEventListener('click', toggleSideMenu);
+
