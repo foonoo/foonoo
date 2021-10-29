9003f6c2b8b55d58865da4e8261b556f5f6dd9ea
Ekow Abaka Ainooson <jainooson@gmail.com>
2021-10-29 17:43:33 -0500

    Feeling better about the stylesheets now

--- themes/site/assets/js/site.js
+++ themes/site/assets/js/site.js
@@ -1,7 +1,9 @@
 let sideToc = document.getElementById('side-toc');
-let modal = document.getElementById('side-toc-modal');
+let modalBackgrounds = document.querySelectorAll(".modal");
 let tocHamburger = document.getElementById('toc-hamburger');
 let menuHamburger = document.getElementById('menu-hamburger');
+let topMenu = document.getElementById('main-navigation');
+let currentModal = {menu: null, button: null, modalBackground: null};
 
 function resizeSideToc() {
      
@@ -11,20 +13,43 @@ function resizeSideToc() {
     let height = window.innerHeight || document.body.clientHeight;
     height = height - document.getElementById('banner-wrapper').clientHeight;
     sideToc.style.height = height + 'px';
-    modal.style.height = height + 'px';
+    modalBackgrounds.forEach(x => x.style.height = height + 'px');
 }
 
-function toggleSideToc() {
-    sideToc.classList.toggle("active");
-    modal.classList.toggle("active");
-    tocHamburger.classList.toggle('active');
+//function toggleSideToc() {
+//    sideToc.classList.toggle("active");
+//    modalBackground.classList.toggle("active");
+//    tocHamburger.classList.toggle('active');
+//}
+
+function toggleMenu(menu, modalBackground) {
+    return function(e) {
+        menu.classList.toggle("active");
+        e.target.classList.toggle('active');
+        modalBackground.classList.toggle('active');
+        if(currentModal.menu !== null && currentModal.menu !== menu) {
+            currentModal.menu.classList.toggle('active');
+            currentModal.button.classList.toggle('active');
+            currentModal.modalBackground.classList.toggle('active');
+        }
+        currentModal = {menu: menu, button: e.target, modalBackground: modalBackground};
+    }
 }
 
 if(sideToc !== null) {
     window.addEventListener('load', resizeSideToc);
     window.addEventListener('resize', resizeSideToc);
-
-    tocHamburger.addEventListener('click', toggleSideToc);
-    modal.addEventListener('click', toggleSideToc);    
+    tocHamburger.addEventListener('click', toggleMenu(sideToc, document.getElementById('side-toc-modal')));//toggleSideToc);
 }
 
+if(topMenu !== null) {
+    menuHamburger.addEventListener('click', toggleMenu(topMenu, document.getElementById('menu-modal')));
+}
+
+modalBackgrounds.forEach(x => x.addEventListener('click', e => {
+        currentModal.button.classList.toggle("active");
+        currentModal.menu.classList.toggle("active");
+        e.target.classList.toggle("active");
+        currentModal = {menu: null, button: null, modalBackground: null}
+    })
+);    
